c3769febad3124fdb7e457c97ae409ffb4880999
choub <piou@hostmail.fr>
2025-04-25 16:17:55 +0200

    Feat(js/css): change style of card on hover, add style button card protolio, the portfolio page is good for now

--- src/components/portfolioPage/Card.js
+++ src/components/portfolioPage/Card.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Button from '../global/Button';
 import { dataCards } from './dataCards';
+import { useNavigate } from 'react-router-dom';
+import './buttonCard.css';
 
 const Card = () => {
+  const navigate = useNavigate();
+
+  const redirect = () => {
+    navigate('/');
+  };
+
   return (
     <div className="container">
       <div className="row row-cols-1 row-cols-md-3 g-4 mb-4">
         {dataCards.map((cards, index) => (
-          <div className="col">
-            <div className="card col text-center mb-3" key={index}>
+          <div className="col" key={index}>
+            <div className="card col text-center mb-3 h-100">
               <img src={cards.img.src} className="card-img-top" alt={cards.img.alt} />
               <div className="card-body px-0 pb-0">
                 <h5 className="card-title">{cards.title}</h5>
                 <p className="card-text mb-0">{cards.text}</p>
-                <Button color="primary mb-3">Voir le Site</Button>
+                <Button color="primary mb-3 lighten-on-hover" onClick={redirect}>
+                  Voir le Site
+                </Button>
                 <div className="card-footer py-3">
                   <p className="mb-0">{cards.footer}</p>
                 </div>
