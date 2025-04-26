04d9712bcdf87779d485e8d2e29849dbd88b99cd
choub <piou@hostmail.fr>
2025-04-26 15:59:05 +0200

    Feat (js/css): Change marding-bottom of main, add icone of contactData.js and PersonalInfo.js, Accordion is end, dataAccordion ok for the moment

--- src/components/global/PersonalInfo.js
+++ src/components/global/PersonalInfo.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const PersonalInfo = ({ street, city, phone, email }) => {
+//withIcons sert uniquement à afficher les icons si il est déclaré.
+
+const PersonalInfo = ({
+  street,
+  city,
+  phone,
+  email,
+  iconeStreet,
+  iconeCity,
+  iconePhone,
+  iconeEmail,
+  withIcons,
+}) => {
   return (
-    <div>
-      <p className="mb-0">{street}</p>
-      <p className="mb-0">{city}</p>
-      <p className="mb-0">{phone}</p>
-      <p className="mb-0">{email}</p>
+    <div className="pt-1">
+      <p className={`mb-0 ${withIcons ? iconeStreet : ' '}`}> {street}</p>
+      <p className={`mb-0 ${withIcons ? iconeCity : ' '}`}> {city}</p>
+      <p className={`mb-0 ${withIcons ? iconePhone : ' '}`}> {phone}</p>
+      <p className={`mb-0 ${withIcons ? iconeEmail : ' '}`}> {email}</p>
     </div>
   );
 };
--- src/components/legalNotices/Accordion.js
+++ src/components/legalNotices/Accordion.js
@@ -3,30 +3,28 @@ import { dataAccordion } from './dataAccordion';
 
 const Accordion = () => {
   return (
-    <div>
-      <div className="accordion" id="accordionExample">
+    <div className="container">
+      <div className="accordion row px-3" id="accordionExample">
         {dataAccordion.map(({ id, header, content }, index) => (
-          <div>
-            <div className="accordion-item" key={id}>
-              <h2 className="accordion-header">
-                <button
-                  className={`accordion-button ${index !== 0 ? 'collapsed' : ''} `}
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target={`#collapse${id}`}
-                  aria-expanded={`${index === 0 ? 'true' : 'false'}`}
-                  aria-controls={`#collapse${id}`}
-                >
-                  {header}
-                </button>
-              </h2>
-              <div
-                id={`#collapse${id}`}
-                className={`accordion-collapse collapse ${index === 0 ? 'show' : ' '}`}
-                data-bs-parent="#accordionExample"
+          <div className="accordion-item px-0" key={id}>
+            <h2 className="accordion-header">
+              <button
+                className={`accordion-button ${index !== 0 ? 'collapsed' : ''} `}
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target={`#collapse${id}`}
+                aria-expanded={index === 0 ? 'true' : 'false'}
+                aria-controls={`collapse${id}`}
               >
-                <div className="accordion-body">{content}</div>
-              </div>
+                {header}
+              </button>
+            </h2>
+            <div
+              id={`collapse${id}`}
+              className={`accordion-collapse collapse ${index === 0 ? 'show' : ' '}`}
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">{content}</div>
             </div>
           </div>
         ))}
