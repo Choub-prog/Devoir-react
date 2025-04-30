70f0abe435c2f784ab356c221da74c0b66a794f2
choub <piou@hostmail.fr>
2025-04-30 20:25:51 +0200

    Feat(js/css): Add other for the button class, FormContact.js is ok for the moment

--- src/components/global/Button.js
+++ src/components/global/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Button = ({ type, color, onClick, children }) => {
+const Button = ({ type, color, onClick, children, other }) => {
   return (
     <button
       type={type || 'button'}
-      className={`btn btn-${color} mt-3 px-4 py-2`}
+      className={`btn btn-${color} mt-3 px-4 py-2 ${other}`}
       onClick={onClick}
     >
       {children}
